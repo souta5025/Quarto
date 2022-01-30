036f4afb4f91e1a31af2697f38bf4df352db7f1f
souta5025 <souta5025y@gmail.com>
2022-01-31 01:06:31 +0900

    選択した駒を選択したボードに置けるようにした

--- Assets/Scripts/BoardSelect.cs
+++ Assets/Scripts/BoardSelect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoardSelect : MonoBehaviour
 {
-    GameManagerScript script; 
+    GameManagerScript script;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +14,22 @@ public class BoardSelect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     //入ってきた時
-    private void OnMouseEnter() {
+    private void OnMouseEnter()
+    {
         transform.localScale *= 1.2f;
     }
     //出ていったとき
-    private void OnMouseExit() {
+    private void OnMouseExit()
+    {
         transform.localScale /= 1.2f;
     }
     //オブジェクト上で話した時
-    private void OnMouseUpAsButton() {
-        
+    private void OnMouseUpAsButton()
+    {
+        var position = this.transform.position;
+        script.piece.transform.position = position;
     }
 }
