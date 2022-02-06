bd3f855dbebc8163797a8a17fb2ee620e59c42b9
souta5025 <souta5025y@gmail.com>
2022-02-07 03:06:36 +0900

    SceneManagerが残るようにした

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -27,4 +27,30 @@ public class SceneChange : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    // GameControllerインスタンスの実体
+    private static SceneChange instance = null;
+
+    // GameControllerインスタンスのプロパティーは、実体が存在しないとき（＝初回参照時）実体を探して登録する
+    public static SceneChange Instance => instance
+        ?? ( instance = GameObject.Find( "SceneManager" ).GetComponent<SceneChange>());
+
+    private void Awake ()
+    {
+        // もしインスタンスが複数存在するなら、自らを破棄する
+        if ( this != Instance )
+        {
+            Destroy ( this.gameObject );
+            return;
+        }
+
+        // 唯一のインスタンスなら、シーン遷移しても残す
+        DontDestroyOnLoad ( this.gameObject );
+    }
+
+    private void OnDestroy ()
+    {
+        // 破棄時に、登録した実体の解除を行う
+        if ( this == Instance ) instance = null;
+    }
 }
